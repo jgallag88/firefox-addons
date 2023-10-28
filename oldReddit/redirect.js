08c95f0392590ff48b3ae18bd32af9f80d37931a
John Gallagher <john@gllghr.com>
2023-10-28 16:17:05 -0700

    Load i.redd.it and preview.redd.it images directly

--- oldReddit/redirect.js
+++ oldReddit/redirect.js
@@ -11,3 +11,24 @@ browser.webRequest.onBeforeRequest.addListener(
     {urls:[pattern], types:["main_frame"]},
     ["blocking"]
 );
+
+/*
+ * If you try to view an image hosted on i.redd.it or preview.redd.it, the reponse will try to redirect to some
+ * page with the image embedded if request header specifies that it accepts HTML. This breaks if the above
+ * redirection logic is present. Simplest to just not accept HTML, in which case the reponse will containe the
+ * image without any redirections.
+ */
+function modifyReddItHeaders(requestDetails) {
+    for (let header of requestDetails.requestHeaders) {
+        if (header.name.toLowerCase() === "accept") {
+            header.value = "*/*";
+	}
+    }
+    return { requestHeaders: requestDetails.requestHeaders };
+}
+
+browser.webRequest.onBeforeSendHeaders.addListener(
+    modifyReddItHeaders,
+    {urls:["*://i.redd.it/*", "*://preview.redd.it/*"], types:["main_frame"]},
+    ["blocking", "requestHeaders"]
+);
