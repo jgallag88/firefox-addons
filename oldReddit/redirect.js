a43643e3a5799350cfd1e3bfdab5b04828114edc
John Gallagher <john@gllghr.com>
2025-02-16 15:09:30 -0800

    Handle 'share' URLs

--- oldReddit/redirect.js
+++ oldReddit/redirect.js
@@ -1,6 +1,19 @@
 var pattern = "*://www.reddit.com/*";
 
+function isShareUrl(url) {
+    // 'Share' URLs have the form https://www.reddit.com/r/<subreddit>/s/<id>
+    return /^\/r\/[^\/]+\/s\//.test(url.pathname);
+}
+
 function redirect(requestDetails) {
+    /*
+     * old.reddit.com doesn't support share URLs, so need to let the redirect happen
+     * before rewritting the URL.
+     */
+    if (isShareUrl(new URL(requestDetails.url))) {
+        return {};
+    }
+
     return {
         redirectUrl: requestDetails.url.replace("://www.reddit.com", "://old.reddit.com")
     };
@@ -15,7 +28,7 @@ browser.webRequest.onBeforeRequest.addListener(
 /*
  * If you try to view an image hosted on i.redd.it or preview.redd.it, the reponse will try to redirect to some
  * page with the image embedded if request header specifies that it accepts HTML. This breaks if the above
- * redirection logic is present. Simplest to just not accept HTML, in which case the reponse will containe the
+ * redirection logic is present. Simplest to just not accept HTML, in which case the reponse will contain the
  * image without any redirections.
  */
 function modifyReddItHeaders(requestDetails) {
